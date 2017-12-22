15c2a51ae6956a03c40ce6be70a173ec7fd5c595
Erfan Mostafid <34257315+erfan1981@users.noreply.github.com>
2017-12-21 16:48:32 -0800

    Update USCounties.R

--- USCounties.R
+++ USCounties.R
@@ -2,7 +2,16 @@ library(geojsonio)
 USCounties <- geojsonio::geojson_read("https://raw.githubusercontent.com/erfan1981/JSON/master/USCounties.geojson")
 pal <- colorNumeric("viridis", NULL)
 library(leaflet)
-leaflet() %>% 
+library(jsonlite)
+
+m <- leaflet() %>% 
 	addTiles() %>% 
 	setView(lng = -98.583, lat = 39.833, zoom = 3) %>% 
-	addGeoJSON(USCounties, weight = 0.5) #, fill = pal( )
+	addTiles() %>% 
+	addGeoJSON(USCounties, weight = 0.5)  #, fill = pal(USCounties$features[[1:3220]]$properties$AWATER)) #$properties$AWATER) $features[[3218]]$properties$AWATER
+ 	#addPolygons() # fillColor = ~pal(AWATER)
+#m %>% addPolygons(lng = -98.583, lat = 39.833) #, fillColor = ~pal(features$properties$AWATER))
+#m %>% addPolygons(fillColor = ~pal(features$properties$AWATER))
+a <- 1:length(USCounties$features)
+for (i in 1:length(USCounties$features)) (a[i] <- USCounties$features[[i]]$properties$AWATER)
+m %>% addPolygons(lng = -98.583, lat = 39.833, fillColor = ~pal(a))
