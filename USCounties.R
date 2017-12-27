f7ac08712e00f2d7b0a25cbff274d287cc9782a2
Erfan Mostafid <34257315+erfan1981@users.noreply.github.com>
2017-12-27 10:29:52 -0800

    Update USCounties.R

--- USCounties.R
+++ USCounties.R
@@ -1,17 +1,9 @@
-library(geojsonio)
-USCounties <- geojsonio::geojson_read("https://raw.githubusercontent.com/erfan1981/JSON/master/USCounties.geojson")
-pal <- colorNumeric("viridis", NULL)
-library(leaflet)
-library(jsonlite)
 
-m <- leaflet() %>% 
-	addTiles() %>% 
-	setView(lng = -98.583, lat = 39.833, zoom = 3) %>% 
-	addTiles() %>% 
-	addGeoJSON(USCounties, weight = 0.5)  #, fill = pal(USCounties$features[[1:3220]]$properties$AWATER)) #$properties$AWATER) $features[[3218]]$properties$AWATER
- 	#addPolygons() # fillColor = ~pal(AWATER)
-#m %>% addPolygons(lng = -98.583, lat = 39.833) #, fillColor = ~pal(features$properties$AWATER))
-#m %>% addPolygons(fillColor = ~pal(features$properties$AWATER))
-a <- 1:length(USCounties$features)
-for (i in 1:length(USCounties$features)) (a[i] <- USCounties$features[[i]]$properties$AWATER)
-m %>% addPolygons(lng = -98.583, lat = 39.833, fillColor = ~pal(a))
+USCounties <- geojsonio::geojson_read("http://eric.clst.org/wupl/Stuff/gz_2010_us_050_00_20m.json",   what = "sp")
+
+pal <- colorNumeric("viridis", NULL)
+
+leaflet(USCounties) %>%
+  	addTiles() %>%
+	setView(lng = -120, lat = 50, zoom = 4) %>% 
+  	addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 1, fillColor = ~pal(log10(as.numeric(CENSUSAREA))))
