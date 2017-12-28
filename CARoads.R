3bb28d61bdf120d30838a2ef8198821c89b2b807
Erfan Mostafid <34257315+erfan1981@users.noreply.github.com>
2017-12-28 12:16:12 -0800

    Update CARoads.R

--- CARoads.R
+++ CARoads.R
@@ -1,5 +1,6 @@
 CARoads <- geojsonio::geojson_read("https://raw.githubusercontent.com/erfan1981/JSON/master/Caltrans_Traffic_Data.geojson",   what = "sp")
-leaflet(CARoads) %>%
+m <- leaflet(CARoads) %>%
   	addTiles() %>%
 	setView(lng = -120, lat = 37, zoom = 7) %>% 
   	addPolylines(stroke = TRUE,  color = ~colorQuantile("YlOrRd", Hrs_Delay_)(Hrs_Delay_))
+m %>% addProviderTiles(providers$CartoDB.Positron)
