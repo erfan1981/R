7b0d7e1edfcefd8775eb2dd33d06a7a8b9de739f
Erfan Mostafid <34257315+erfan1981@users.noreply.github.com>
2017-12-28 12:20:37 -0800

    Update CARoads.R

--- CARoads.R
+++ CARoads.R
@@ -1,3 +1,6 @@
+# Annual Vehicle Hours of Delay (VHD) at 35 miles per hour (mph)
+# Delay data from CALTRANS: https://data.ca.gov/node/271/api
+# Road Shapefile Vector data from CALTRANS: http://www.dot.ca.gov/hq/tsip/gis/datalibrary/#Highway
 CARoads <- geojsonio::geojson_read("https://raw.githubusercontent.com/erfan1981/JSON/master/Caltrans_Traffic_Data.geojson",   what = "sp")
 m <- leaflet(CARoads) %>%
   	addTiles() %>%
