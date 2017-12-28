16e163c877a716d235721d060809aaebefa20677
Erfan Mostafid <34257315+erfan1981@users.noreply.github.com>
2017-12-27 16:02:26 -0800

    Update CARoads.R

--- CARoads.R
+++ CARoads.R
@@ -1,3 +1,4 @@
+
 CARoads <- geojsonio::geojson_read("https://raw.githubusercontent.com/erfan1981/JSON/master/Caltrans_Traffic_Data.geojson",   what = "sp")
 
 pal <- colorNumeric("viridis", NULL)
@@ -5,4 +6,4 @@ pal <- colorNumeric("viridis", NULL)
 leaflet(CARoads) %>%
   	addTiles() %>%
 	setView(lng = -120, lat = 50, zoom = 4) %>% 
-  	addPolygons()# stroke = FALSE, smoothFactor = 0.3, fillOpacity = 1) #, fillColor = ~pal(as.numeric("Hrs_Delay_")))
+  	addPolylines(fillColor = ~pal(as.numeric("Hrs_Delay_")) # stroke = FALSE, smoothFactor = 0.3, fillOpacity = 1) #,
