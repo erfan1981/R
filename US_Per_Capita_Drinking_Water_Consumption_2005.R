21e50ad053ad6b404859db908377ccd586baea60
Erfan Mostafid <34257315+erfan1981@users.noreply.github.com>
2017-12-29 13:12:53 -0800

    Add files via upload

---
+++ US_Per_Capita_Drinking_Water_Consumption_2005.R
@@ -0,0 +1,10 @@
+# Per cepita driniking water consumption in selected US cities 2005
+# Source: https://www.census.gov/support/USACdataDownloads.html#WAT
+# Coordinates for cities: https://simplemaps.com/data/us-cities
+require(leaflet)
+USWater <- geojsonio::geojson_read("https://raw.githubusercontent.com/erfan1981/JSON/master/US_Drinking_Water_Per_Capita.geojson",   what = "sp")
+m <- leaflet(USWater) %>%
+  	addTiles() %>%
+	setView(lng = -120, lat = 50, zoom = 4) %>% 
+	addCircles(stroke = TRUE,  color = ~colorQuantile("YlOrRd", WAT330205D)(WAT330205D))
+m %>% addProviderTiles(providers$CartoDB.Positron)
