ea4122562d58cafac3c659d0edcd48362f281f16
Erfan Mostafid <34257315+erfan1981@users.noreply.github.com>
2018-01-04 14:23:26 -0800

    Update US_Per_Capita_Drinking_Water_Consumption_2005.R

--- US_Per_Capita_Drinking_Water_Consumption_2005.R
+++ US_Per_Capita_Drinking_Water_Consumption_2005.R
@@ -1,10 +1,22 @@
 # Per capita driniking water consumption in selected US cities 2005
 # Source: https://www.census.gov/support/USACdataDownloads.html#WAT
-# Coordinates for cities: https://simplemaps.com/data/us-cities
+# Coordinates for cities: https://simplemaps.com/data/us-cities- CHECK DATA SOURCE, Water Consumtions are too high for some places
+
+pal <- colorNumeric(
+  palette = "YlOrRd",
+  domain = USWater$WAT330205D
+)
+
 require(leaflet)
 USWater <- geojsonio::geojson_read("https://raw.githubusercontent.com/erfan1981/JSON/master/US_Drinking_Water_Per_Capita.geojson",   what = "sp")
 m <- leaflet(USWater) %>%
   	addTiles() %>%
-	setView(lng = -120, lat = 50, zoom = 4) %>% 
+	setView(lng = -120, lat = 50, zoom = 3) %>% 
 	addCircles(stroke = TRUE,  color = ~colorQuantile("YlOrRd", WAT330205D, n = 9)(WAT330205D))
-m %>% addProviderTiles(providers$CartoDB.Positron)
+m %>% addProviderTiles(providers$CartoDB.Positron) %>%
+
+addLegend("bottomleft", pal = pal, values = ~WAT330205D,
+    title = "Est. Per Capita Drinking Water Consumption 2005",
+    labFormat = labelFormat(suffix = " Gallons/Day"),
+    opacity = 1
+  )
