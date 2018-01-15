55579abf4723e79c6eda172c12879d807f3ed381
Erfan Mostafid <34257315+erfan1981@users.noreply.github.com>
2018-01-15 15:18:24 -0800

    Update US_Per_Capita_Drinking_Water_Consumption_2005.R

--- US_Per_Capita_Drinking_Water_Consumption_2005.R
+++ US_Per_Capita_Drinking_Water_Consumption_2005.R
@@ -2,21 +2,20 @@
 # Source: https://www.census.gov/support/USACdataDownloads.html#WAT
 # Coordinates for cities: https://simplemaps.com/data/us-cities
 
-pal <- colorNumeric(
-  palette = "YlOrRd",
-  domain = USWater$WAT330205D
-)
-
 require(leaflet)
 USWater <- geojsonio::geojson_read("https://raw.githubusercontent.com/erfan1981/JSON/master/US_Drinking_Water_Per_Capita.geojson",   what = "sp")
+pal <- colorNumeric(
+  palette = "YlOrRd",
+  domain = USWater$Ave
+)
 m <- leaflet(USWater) %>%
   	addTiles() %>%
 	setView(lng = -120, lat = 50, zoom = 3) %>% 
-	addCircles(stroke = TRUE,  color = ~colorNumeric( palette = "YlOrRd", WAT330205D, n = 9)(WAT330205D))
+	addCircles(stroke = TRUE,  color = ~colorNumeric( palette = "YlOrRd", Ave, n = 9)(Ave))
 m %>% addProviderTiles(providers$CartoDB.Positron) %>%
 
-addLegend("bottomleft", pal = pal, values = ~WAT330205D,
-    title = "Est. Per Capita Drinking Water Consumption 2005",
+addLegend("bottomleft", pal = pal, values = ~Ave,
+    title = "Est. Per Capita Drinking Water Consumption Average 1995-2005",
     labFormat = labelFormat(suffix = " Gallons/Day"),
     opacity = 1
   )
