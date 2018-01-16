aa83333422a4c00c381f66ed84cd51dea25c2bfe
Erfan Mostafid <34257315+erfan1981@users.noreply.github.com>
2018-01-16 15:41:44 -0800

    Add files via upload

---
+++ USWater_Counties.R
@@ -0,0 +1,22 @@
+# Per capita driniking water consumption for US cities averages from 1995 to 2005
+# Source: https://www.census.gov/support/USACdataDownloads.html#WAT
+# Coordinates for cities: https://simplemaps.com/data/us-cities
+
+require(leaflet)
+USWater <- geojsonio::geojson_read("https://raw.githubusercontent.com/erfan1981/JSON/master/USWater1.geojson",   what = "sp")
+pal <- colorNumeric(
+  palette = "YlOrRd",
+  domain = USWater$AveN
+)
+
+m <- leaflet(USWater) %>%
+  	addTiles() %>%
+	setView(lng = -120, lat = 50, zoom = 3) %>% 
+	addPolygons(stroke = TRUE, color = ~colorNumeric( palette = "YlOrRd", AveN, n = 9)(AveN), fill = TRUE, fillColor = ~colorNumeric( palette = "YlOrRd", AveN, n = 9)(AveN) , fillOpacity = 0.6)
+m %>% addProviderTiles(providers$CartoDB.Positron) %>%
+
+addLegend("bottomleft", pal = pal, values = ~AveN,
+    title = "Per Capita Drinking Water Consumption Average 1995-2005",
+    labFormat = labelFormat(suffix = " Gallons/Day"),
+    opacity = 1
+  )
