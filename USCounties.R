5b4274bdb42e4e543a600c6c71b76d920456c511
Erfan Mostafid <34257315+erfan1981@users.noreply.github.com>
2018-01-16 16:25:39 -0800

    Delete USCounties.R

--- USCounties.R
+++
@@ -1,9 +0,0 @@
-
-USCounties <- geojsonio::geojson_read("http://eric.clst.org/wupl/Stuff/gz_2010_us_050_00_20m.json",   what = "sp")
-
-pal <- colorNumeric("viridis", NULL)
-
-leaflet(USCounties) %>%
-  	addTiles() %>%
-	setView(lng = -120, lat = 50, zoom = 4) %>% 
-  	addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 1, fillColor = ~pal(log10(as.numeric(CENSUSAREA))))
