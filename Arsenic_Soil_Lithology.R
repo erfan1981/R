53784653c8f29d809d016249bb735721572b8411
Erfan Mostafid <34257315+erfan1981@users.noreply.github.com>
2018-03-30 14:18:13 -0700

    Update Arsenic_Soil_Lithology.R

--- Arsenic_Soil_Lithology.R
+++ Arsenic_Soil_Lithology.R
@@ -25,6 +25,8 @@ BRSW <- shapiro.test(BR$Result)
 library(nortest)
 FillSF <- sf.test(Fill$Result) # Shapiro-Francia Normality Test for Sample Size > 50
 if (FillSF$p.value < 0.05) {print("The null hypothesis that the data is normally distributed is rejected")} else {print("Cannot reject the null hypothesis that the data is normally distributed")}
+hist(Fill$Result, breaks=500, xlim = c(0,50))
+qqnorm (Fill$Result)
 BMSF <- sf.test(BM$Result)
 BRSF <- sf.test(BR$Result)
 ttest <- t.test(Fill$Result,BM$Result)
